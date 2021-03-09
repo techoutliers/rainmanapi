bbbfd84662aad1554c7c4e6f7cce781770b5dee6
deenMuhammad <zorro199696@gmail.com>
2021-03-09 15:21:53 +0900

    fund clientip

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 router.get("/weather", function (req, res, next) {
-  var ip = req.headers["x-forwarded-for"];
-  console.log(req.headers);
-  console.log(JSON.stringify(ip));
+  var ip = req.headers["x-real-ip"];
   console.log(ip);
   res.status(200).send({ body: "Weather" });
 });
